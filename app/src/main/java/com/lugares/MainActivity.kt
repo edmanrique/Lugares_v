e8eff99edaa5a3aa65f496dbf1a1a702fba9165b
Edison <edisonthomas1503@gmail.com>
2022-01-28 21:04:36 -0600

    Semana 01

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.lugares
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugares.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //Acá haremos algo
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btLogin.setOnClickListener { haceLogin() }
+        binding.btRegister.setOnClickListener { haceRegister() }
+
+    }
+
+    private fun haceRegister() {
+        val email = binding.etEmail.text.toString()
+        val clave = binding.etClave.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, clave)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+                    Log.d("Autenticando", "Usuario creado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Log.d("Autenticando", "Ha ocurrido un error. Usuario no creado")
+                    actualiza(null)
+                }
+            }
+    }
+
+    private fun actualiza(user : FirebaseUser?) {
+        if(user != null){
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun haceLogin() {
+        val email = binding.etEmail.text.toString()
+        val clave = binding.etClave.text.toString()
+
+        auth.signInWithEmailAndPassword(email, clave)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful){
+                    Log.d("Autenticando", "Usuario logueado")
+                    val user = auth.currentUser
+                    actualiza(user)
+                } else {
+                    Log.d("Autenticando", "Usuario incorrecto")
+                    actualiza(null)
+                }
+            }
     }
 }
