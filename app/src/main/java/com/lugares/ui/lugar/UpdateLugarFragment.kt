56b27b11730b22afa439eb8abe57f143087874d7
edmanriquer_ <emanrique80713@ufide.ac.cr>
2022-02-25 20:52:15 -0600

    Semana 05

--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,6 +1,9 @@
 package com.lugares.ui.lugar
 
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
@@ -12,6 +15,7 @@ import com.lugares.R
 import com.lugares.databinding.FragmentUpdateLugarBinding
 import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
+import java.util.jar.Manifest
 
 class UpdateLugarFragment : Fragment() {
 
@@ -33,13 +37,103 @@ class UpdateLugarFragment : Fragment() {
         binding.etCorreo.setText(args.lugar.correo)
         binding.etTelefono.setText(args.lugar.telefono)
         binding.etWeb.setText(args.lugar.web)
+        binding.tvLongitud.text = args.lugar.longitud.toString()
+        binding.tvLatitud.text = args.lugar.latitud.toString()
+        binding.tvAltura.text = args.lugar.altura.toString()
 
         binding.btActualizar.setOnClickListener {
             updateLugar()
         }
 
+        binding.btEmail.setOnClickListener {
+            escribirCorreo()
+        }
+
+        binding.btPhone.setOnClickListener {
+            llamarLugar()
+        }
+
+        binding.btWhatsapp.setOnClickListener {
+            enviarWhatsapp()
+        }
+
+        binding.btWeb.setOnClickListener {
+            verWeb()
+        }
+
+        binding.btLocation.setOnClickListener {
+            verMapa()
+        }
+
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    private fun verMapa() {
+        val latitud = binding.tvLatitud.text.toString().toDouble()
+        val longitud = binding.tvLongitud.text.toString().toDouble()
+        if(latitud.isFinite() && longitud.isFinite()){
+            val location = Uri.parse("geo:$latitud, $longitud?z=18")
+            val intent = Intent(Intent.ACTION_VIEW, location)
+
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_datos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun verWeb() {
+        val sitio = binding.etWeb.text.toString()
+        if(sitio.isNotEmpty()){
+            val uri = Uri.parse("http://$sitio")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_datos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun enviarWhatsapp() {
+        val telefono = binding.etTelefono.text.toString()
+        if(telefono.isNotEmpty()){
+            val intent = Intent(Intent.ACTION_VIEW)
+            val uri = "whatsapp://send?phone=506$telefono&text=" + getString(R.string.msg_saludos)
+            intent.setPackage("com.whatsapp")
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_datos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun llamarLugar() {
+        val telefono = binding.etTelefono.text.toString()
+        if(telefono.isNotEmpty()){
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$telefono")
+            if(requireActivity().checkSelfPermission(android.Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                requireActivity().requestPermissions(arrayOf(android.Manifest.permission.CALL_PHONE), 105)
+            } else {
+                requireActivity().startActivity(intent)
+            }
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_datos), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun escribirCorreo() {
+        val para = binding.etCorreo.text.toString()
+        if(para.isNotEmpty()){
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "message/rfc822"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(para))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.msg_saludos) + " " + binding.etNombre.text)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.msg_mensaje_correo))
+            startActivity(intent)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.msg_datos), Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
